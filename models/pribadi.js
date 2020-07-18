1994c82f4106c1a6f0ea575f64ce191d50755941
anggaes <anggaes1@gmail.com>
2020-07-18 13:42:24 +0700

    Menambah association ke User

--- models/pribadi.js
+++ models/pribadi.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.belongsTo(models.User, {foreignKey: 'userId', as: 'company'})
     }
   };
   Pribadi.init({
