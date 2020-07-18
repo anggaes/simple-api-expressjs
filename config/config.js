1c6aa8f8f60e6f7232e4ade8c9936b22829d2e4d
anggaes <anggaes1@gmail.com>
2020-07-18 11:45:36 +0700

    Install Sequelize-cli, init, and add config.js for Sequelize

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require("dotenv").config()
+
+module.exports = {
+  "development": {
+    "username": process.env.DEV_DB_USER,
+    "password": process.env.DEV_DB_PWD,
+    "database": process.env.DEV_DB_NAME,
+    "host": process.env.DEV_DB_HOST,
+    "dialect": "mariadb"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
