3812aa3bb37cba5f1d590c3fb5d557e85702f257
anggaes <anggaes1@gmail.com>
2020-07-18 13:35:17 +0700

    Generate models:User dan Pribadi dan File Migrasinya

---
+++ migrations/20200718061822-create-pribadi.js
@@ -0,0 +1,47 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Pribadis', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nik: {
+        type: Sequelize.STRING
+      },
+      nama: {
+        type: Sequelize.STRING
+      },
+      gender: {
+        type: Sequelize.STRING
+      },
+      tglLahir: {
+        type: Sequelize.STRING
+      },
+      tptLahir: {
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {         // User belongsTo Company 1:1
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Pribadis');
+  }
+};
---
+++ models/pribadi.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Pribadi extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Pribadi.init({
+    nik: DataTypes.STRING,
+    nama: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    tglLahir: DataTypes.STRING,
+    tptLahir: DataTypes.STRING,
+    userId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Pribadi',
+  });
+  return Pribadi;
+};
