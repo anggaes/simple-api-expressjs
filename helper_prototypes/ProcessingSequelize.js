f91c58ceb7f6cf1883da71c0dc7f9b973746b00f
anggaes <anggaes1@gmail.com>
2020-07-21 09:47:14 +0700

    Add ProcessingSequelize as an Prototype for Sequelize Result

---
+++ helper_prototypes/ProcessingSequelize.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const ProcessingSequelize = Object.create({
+  init: function(dataFromSequelize,protos){
+    this.dataFromSequelize = dataFromSequelize;
+    this.protos = protos;
+    this.resultSerialization;
+    return this;
+  },
+  get _isInitialized() {
+    if(this.dataFromSequelize && this.protos)
+      return true;
+    else{
+      return false;
+    }
+  },
+  serializeOneRow: function(){
+    if(!this._isInitialized) 
+        throw new Error('ProcessingSequelize is not initialized yet');
+    this.resultSerialization = this._serializeOne(this.dataFromSequelize,this.protos);
+    return this;
+  },
+  _serializeOne: function(dataFromSequelize,protos){
+    let i = 0;
+    function serializeRecursive(dataFromSequelize,protos){
+      let bucketOfObject = Object.create(protos[i]);
+      i++;
+      if(dataFromSequelize.dataValues)
+      Object.keys(dataFromSequelize.dataValues).map(function(key, index) {
+        if(dataFromSequelize.dataValues[key] !== null && dataFromSequelize.dataValues[key].dataValues && typeof dataFromSequelize.dataValues[key] === "object"){
+          bucketOfObject[key] = serializeRecursive(dataFromSequelize.dataValues[key],protos);
+        }else
+          bucketOfObject[key] = dataFromSequelize.dataValues[key];   
+      });
+      return bucketOfObject
+    }
+    return serializeRecursive(dataFromSequelize,protos)
+  },
+  serializeMultiRow: async function(){
+    if(!this._isInitialized) 
+      throw new Error('ProcessingSequelize is not initialized yet');
+    if(!Array.isArray(this.dataFromSequelize))
+      throw new Error('Data from Sequelize must be an Array');
+
+    let result = []
+    const _serializeOne = this._serializeOne
+    const protos = this.protos
+    
+    await this.dataFromSequelize.map(async function(value, key, index) {
+      result.push(await _serializeOne(value,protos))
+    });
+    this.resultSerialization = result;
+
+    return this;
+  },
+  // setGender: function(gender){
+  //     if(!this._isInitialized) 
+  //         throw new Error('Pribadi is not initialized yet');
+  //     this.gender = gender;
+  //     return this
+  // }
+});
+
+
+module.exports = ProcessingSequelize;
