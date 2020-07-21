e3d9d2116b43a930943f170472e33443cf31e9c7
anggaes <anggaes1@gmail.com>
2020-07-21 09:48:34 +0700

    Change how make instance of class to make prototype

--- prototype_collections/PribadiProto.js
+++ prototype_collections/PribadiProto.js
@@ -1,15 +1,40 @@
-const Pribadi = Object.create({
-	_nik: '',
-	_nama:'',
-	_gender:'',
-    _tglLahir:'',
-    _tptLahir:'',
-    _user:{},
-    printTglLahir : function()	{
-    	console.log("Angga")
-    	console.log(this._tglLahir)
-    }
+'use strict';
 
+const Pribadi = Object.create({
+  // isInitialized: false,
+  init: function(nik,nama){
+      // this.isInitialized = true
+      this.nik = nik;
+      this.nama = nama;
+      return this;
+  },
+  get _isInitialized() {
+        if(this.nik && this.nama)
+          return true;
+        else{
+          return false;
+        }
+  },
+  setGender: function(gender){
+      if(!this._isInitialized) 
+          throw new Error('Pribadi is not initialized yet');
+      this.gender = gender;
+      return this
+  },
+  setTglLahir: function(tglLahir){
+      if(!this._isInitialized) 
+          throw new Error('Pribadi is not initialized yet');
+      this.tglLahir = tglLahir;
+      return this
+  },
+  setTptLahir: function(tglLahir){
+      if(!this._isInitialized) 
+          throw new Error('Pribadi is not initialized yet');
+      this.tptLahir = tptLahir;
+      return this
+  },
+  user:{},
 });
+
 
 module.exports = {Pribadi};
--- prototype_collections/UserProto.js
+++ prototype_collections/UserProto.js
@@ -1,6 +1,27 @@
+'use strict';
+
 const User = Object.create({
-	_userName: '',
-	_email:''
+  // isInitialized: false,
+  init: function(userName,email){
+    // this.isInitialized = true
+    this.userName = userName;
+    this.email = email;
+    return this;
+  },
+  get _isInitialized() {
+    if(this.userName && this.email)
+      return true;
+    else{
+      return false;
+    }
+  },
+  setuserName: function(userName){
+      if(!this._isInitialized) 
+          throw new Error('User is not initialized yet');
+      this.userName = userName;
+      return this
+  },
 });
+
 
 module.exports = {User};
