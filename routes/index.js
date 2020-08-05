f0b841b485c8aa1881865bdab081e344eec814f2
anggaes <anggaes1@gmail.com>
2020-08-05 10:28:31 +0700

    Add handler for deleting route

--- routes/index.js
+++ routes/index.js
@@ -26,3 +26,10 @@ exports.create = async (req,res,next) => {
   const result = await controller.create(req,res);
   res.status(result.status).json({ret: result.ret, data:result.dataResult,message:result.message})
 }
+
+exports.update = async (req,res,next) => {
+  const model = extractModelFromRequest(req);
+  let controller = new Controller({model:model})
+  const result = await controller.update(req,res);
+  res.status(result.status).json({ret: result.ret, data:result.dataResult,message:result.message})
+}
