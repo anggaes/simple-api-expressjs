87787392eb9ec6aabd4df6304e7199204c042c7a
anggaes <anggaes1@gmail.com>
2020-08-05 10:26:08 +0700

    Add route for delete

--- app.js
+++ app.js
@@ -2,21 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const router = express.Router();
-// const logger = require("./helper/logger");
 const isProduction = process.env.NODE_ENV == "production" ? true : false;
 
 const models = require('./models');
 const routes = require('./routes');
 
 // APP
-// const compression = require("compression");
-// const helmet = require("helmet");
-// const cors = require("cors");
 app.disable("x-powered-by");
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false }));
 
-// console.log(models)
 delete models.sequelize
 delete models.Sequelize
 
@@ -37,95 +32,13 @@ Object.keys(models).map(function(key,value){
   app.get("/"+key.toLowerCase()+"/:id", splitIncludeInQuery, routes.findOne);
   app.get("/"+key.toLowerCase()+"/", splitIncludeInQuery, routes.findAll);
   app.post("/"+key.toLowerCase()+"/", splitIncludeInQuery, routes.create);
+  app.put("/"+key.toLowerCase()+"/:id", splitIncludeInQuery, routes.update);
 })
 
 
 
 //ROUTER (END)
 
-
-// app.use(cors());
-// app.options("*", cors());
-// app.use(compression());
-// app.use(helmet());
-
-// Request Transformer && Response Transformer
-// const mung = require("express-mung");
-// const crypto = require("./helper/crypto");
-// const rawBaseUrlList = ["/smile"];
-// const rawPathList = [];
-// if (isProduction) {
-//   app.use(function (req, res, next) {
-//     // skip some paths
-//     if (
-//       req.baseUrl.includes(rawBaseUrlList) ||
-//       rawPathList.includes(req.path)
-//     ) {
-//       next();
-//       return;
-//     }
-
-//     // decrypt if has data
-//     if (req.body.data) {
-//       const decryptedData = crypto.decrypt(req.body.data);
-//       const dataObj = JSON.parse(decryptedData);
-//       req.body = dataObj;
-//       //console.log('transformed obj:')
-//       //console.log(req.body)
-//     }
-//     next();
-//   });
-//   app.use(
-//     mung.json(function transform(body, req, res) {
-//       // log before send
-//       logger.info(
-//         req.method +
-//         " " +
-//         req.url +
-//         " - req = " +
-//         JSON.stringify(req.body) +
-//         ", res = " +
-//         JSON.stringify({
-//           ret: body.ret,
-//           msg: body.msg,
-//         })
-//       );
-
-//       // skip some paths
-//       if (
-//         req.baseUrl.includes(rawBaseUrlList) ||
-//         rawPathList.includes(req.path)
-//       ) {
-//         return body;
-//       }
-
-//       //encrypt
-//       const encryptedData = crypto.encrypt(JSON.stringify(body));
-//       body = { data: encryptedData };
-//       return body;
-//     })
-//   );
-// } else {
-//   app.use(
-//     mung.json(function log(body, req, res) {
-//       logger.info(
-//         req.method +
-//         " " +
-//         req.url +
-//         " - req = " +
-//         JSON.stringify(req.body) +
-//         ", res = " +
-//         JSON.stringify({
-//           ret: body.ret,
-//           msg: body.msg,
-//         })
-//       );
-//       return body;
-//     })
-//   );
-// }
-
-// ROUTER
 
 
 // Error Handler
@@ -148,6 +61,5 @@ app.use(function (err, req, res, next) {
   return res.status(500).json({ status: 500, message: err.message });
 });
 
-// SwaggerUI
 
 module.exports = app;
